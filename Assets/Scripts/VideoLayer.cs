8a49a261568e700fb77a85a9c086d7ec3954042a
Ansh Patel <lightnarcissus@gmail.com>
2021-10-21 16:21:19 -0400

    Added basic video compositing scripts

---
+++ Assets/Scripts/VideoLayer.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoLayer : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public VideoClip vidClip;
+    public RawImage rawImage;
+
+    //colors
+    private Color visibleColor = new Color(1, 1, 1, 1);
+    private Color hiddenColor = new Color(0,0,0,0);
+
+
+
+    private void Awake()
+    {
+        rawImage.color = hiddenColor; //transparent by default
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(gameObject.name == "BACKGROUND")
+            UnityEngine.Debug.Log("playback time " + videoPlayer.time.ToString());
+    }
+
+    public IEnumerator PrepareVideoTexture()
+    {
+        if (videoPlayer == null || rawImage == null)
+            yield break;
+
+
+        //yield return StartCoroutine(VideoLayerManager.Instance.AddToActiveVideoLayer(this));
+
+        UnityEngine.Debug.Log("preparing  " + gameObject.name);
+        videoPlayer.clip = vidClip;
+        videoPlayer.renderMode = VideoRenderMode.APIOnly;
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+            yield return new WaitForSeconds(1);
+
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        videoPlayer.Pause();
+        yield return null;
+    }
+    //this usually corresponds to the layer being added to the "active stack"
+    public IEnumerator BeginPlayback()
+    {
+        UnityEngine.Debug.Log("beginning playback of " + gameObject.name);
+
+        videoPlayer.Play();
+        yield return null;
+    }
+
+    public void ToggleLayerVisibility(bool isVisible)
+    {
+        UnityEngine.Debug.Log("making " + gameObject.name + " visibility: " + isVisible.ToString());
+        rawImage.color = (isVisible) ? visibleColor : hiddenColor;
+    }
+
+    public double GetPlaybackTime()
+    {
+        return videoPlayer.time;
+    }    
+
+    public IEnumerator TogglePlayback(bool shouldPause)
+    {
+        if (shouldPause)
+            videoPlayer.Pause();
+        else
+            videoPlayer.Play();
+        yield return null;
+    }
+
+    public IEnumerator ScrollToPlaybackTime(double playbackTime)
+    {
+        videoPlayer.time = playbackTime;
+        yield return null;
+    }
+
+    public IEnumerator AbortPlayback()
+    {
+        //videoPlayer.Stop();
+        //rawImage.color = hiddenColor;
+        yield return null;
+    }
+}
---
+++ Assets/VideoLayerManager.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoLayerManager : MonoBehaviour
+{
+    List<VideoLayer> layerList;
+
+
+    //EXPERIMENT IS A SINGLETON
+    private static VideoLayerManager _instance;
+    public static VideoLayerManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public VideoLayer backgroundLayer;
+    public VideoLayer itemLayer;
+
+
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            UnityEngine.Debug.Log("Instance already exists!");
+            return;
+        }
+        _instance = this;
+
+       
+        layerList = new List<VideoLayer>();
+        
+        StartCoroutine("SetupLayers");
+        StartCoroutine("RunSpawnProcedure");
+
+        //StartCoroutine("PrepareForMatchProcedure");
+    }
+
+    IEnumerator SetupLayers()
+    {
+        yield return StartCoroutine(AddToActiveVideoLayer(backgroundLayer, true));
+        yield return StartCoroutine(AddToActiveVideoLayer(itemLayer, false)); //item layer is not visible by default
+        yield return null;
+    }
+
+    IEnumerator PrepareForMatchProcedure()
+    {
+
+        yield return StartCoroutine(AddToActiveVideoLayer(backgroundLayer, false));
+        yield return StartCoroutine(AddToActiveVideoLayer(itemLayer, false));
+        yield return null;
+    }
+
+    IEnumerator MatchProcedure()
+    {
+
+
+        float randPlaybackTime = Random.Range(0f, (float)backgroundLayer.vidClip.length);
+        UnityEngine.Debug.Log("moving to playback time " + randPlaybackTime.ToString());
+        yield return StartCoroutine(backgroundLayer.ScrollToPlaybackTime(randPlaybackTime));
+        yield return StartCoroutine(itemLayer.ScrollToPlaybackTime(randPlaybackTime));
+        yield return null;
+    }
+
+    IEnumerator RunSpawnProcedure()
+    {
+        //backgroundLayer.ToggleLayerVisibility(true);
+        //yield return AddToActiveVideoLayer(backgroundLayer,true);
+        yield return null;
+    }
+
+
+    IEnumerator SpawnItem()
+    {
+        //pause all other active layers; mostly the background
+        //yield return AddToActiveVideoLayer(itemLayer,true);
+
+        itemLayer.ToggleLayerVisibility(true);
+
+        yield return StartCoroutine("TogglePauseLayerPlayback", true);
+        double playbackTime = backgroundLayer.GetPlaybackTime();
+        yield return StartCoroutine(itemLayer.ScrollToPlaybackTime(playbackTime));
+        yield return StartCoroutine(backgroundLayer.ScrollToPlaybackTime(playbackTime));
+        UnityEngine.Debug.Log("moving to playback time " + playbackTime.ToString());
+        yield return new WaitForSeconds(1.5f); //presentation/animation time
+        yield return StartCoroutine(RemoveVideoLayer(itemLayer));
+
+        yield return StartCoroutine("TogglePauseLayerPlayback", false);
+        yield return null;
+    }
+
+
+
+    IEnumerator RemoveVideoLayer(VideoLayer layer)
+    {
+        layer.ToggleLayerVisibility(false);
+        //layerList.Remove(layer);
+        //yield return StartCoroutine(layer.AbortPlayback());
+        yield return null;
+    }
+
+
+     public IEnumerator AddToActiveVideoLayer(VideoLayer layer,bool isVisible)
+    {
+        layerList.Add(layer);
+
+        UnityEngine.Debug.Log("adding to list " + layerList.Count.ToString());
+        //if (!layer.videoPlayer.isPrepared)
+        //{
+        //    UnityEngine.Debug.Log(layer.gameObject.name + " not prepared; preparing textures");
+            yield return StartCoroutine(layer.PrepareVideoTexture());
+        //}
+
+        //check to see if we should immediately begin playback of the layer
+        if (isVisible)
+            layer.ToggleLayerVisibility(true);
+
+        yield return StartCoroutine(layer.BeginPlayback());
+
+        yield return null;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            StartCoroutine("MatchProcedure");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine("SpawnItem");
+        }
+            if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine("TogglePauseLayerPlayback",true);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine("TogglePauseLayerPlayback",false);
+        }
+    }
+
+    IEnumerator TogglePauseLayerPlayback(bool isPaused)
+    {
+        UnityEngine.Debug.Log("toggling pause " + isPaused.ToString() + " for " + layerList.Count.ToString());
+        for (int i = 0; i < layerList.Count; i++)
+        {
+                yield return StartCoroutine(layerList[i].TogglePlayback(isPaused));
+        }
+        yield return null;
+    }
+
+}
